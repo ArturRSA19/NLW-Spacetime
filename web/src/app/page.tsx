ee39f00fbf9bdb286ea6d96e5aa10348bbc722f2
Artur Rodrigues <artur190503@outlook.com>
2023-05-20 00:52:37 -0300

    Aula 2 - Web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,73 @@
+import { User } from 'lucide-react'
+import Image from 'next/image'
+import logoSpace from '../assets/spacetime-logo.svg'
+
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-900 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Esquerda */}
+      <div className=" relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+        {/* Stripes */}
+        <div className="absolute bottom-0 right-0 top-0 w-2 bg-stripes pr-2" />
+
+        {/* Cadastro */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
+
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={logoSpace} alt="Logo" />
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+          <a
+            href=""
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+
+        {/* Copyright */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito por{' '}
+          <a
+            className="underline hover:text-gray-50"
+            href="https://github.com/ArturRSA19"
+          >
+            Artur!
+          </a>
+        </div>
+      </div>
+
+      {/* Direita */}
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
