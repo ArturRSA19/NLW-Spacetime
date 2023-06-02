54c5db865c603fa7ef5f61391d7f50b11e7cea27
Artur Rodrigues <artur190503@outlook.com>
2023-06-02 19:28:26 -0300

    Aula 3 - Min 57:20

---
+++ web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,15 @@
+import { api } from '@/lib/api'
+import { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
+
+  const registerResponse = await api.post('/register', {
+    code,
+  })
+
+  const { token } = registerResponse.data
+
+  console.log(token)
+}
